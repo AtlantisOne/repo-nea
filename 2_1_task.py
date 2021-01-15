3412bf5818079b10f937f8fdfcccbd2a690dcd1d
Evgen <eanikulin@yandex.ru>
2021-01-15 20:15:36 +0500

    //Homework 2 finish

--- 2_1_task.py
+++ 2_1_task.py
@@ -1,1 +1,8 @@
 # 2_1_task
+
+my_list = [2, 2.4, 4 + 3j, 'My string', [1, 2], (1, 2), {1, 2}, {'key_1': 'val_1', 'key_2': 'val_2'}, (b'text'), True]
+
+element = 0
+for count in my_list:
+    print(type(my_list[element]))
+    element += 1
