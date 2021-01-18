348dad96690dc99e8016f27f437cf71000dc672c
Evgen <eanikulin@yandex.ru>
2021-01-19 00:02:50 +0500

    //Решил все задачи, осталось оптимизировать код

--- 3_2_task.py
+++ 3_2_task.py
@@ -1,1 +1,8 @@
 # 3_2_task
+
+def second_func(name, surname, age, city, email, tel):
+    print(f"Имя - {name}; Фамилия - {surname}; Возраст - {age}; Город - {city}; Email - {email}; Телефон - {tel}")
+
+
+second_func(name=input('Введите Имя:'), surname=input('Введите Фамилию:'), age=input('Введите Возраст:'),
+            city=input('Введите Город:'), email=input('Введите email:'), tel=input('Введите Телефон:'))
--- 3_6_task.py
+++ 3_6_task.py
@@ -1,1 +1,17 @@
 # 3_6_task
+
+# Вариант 1, с запросом у пользователя без аргументов функии
+def int_func(*args):
+    user_answer = input('Введите текст: ')
+    print(user_answer.capitalize())
+
+
+int_func()
+
+
+# Вариант 2, с аргументом функции
+def int_func2(text):
+    print(text.capitalize())
+
+
+print(int_func2('слово'))
