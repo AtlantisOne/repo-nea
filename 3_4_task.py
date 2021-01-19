9d2e39976d6524ae3aa9882e77e1392a2dede618
Evgen <eanikulin@yandex.ru>
2021-01-19 18:13:55 +0500

    //Homework 3 complete

--- 3_4_task.py
+++ 3_4_task.py
@@ -1,6 +1,6 @@
 # 3_4_task
-
 # Варикнт 1, с оператором **
+
 def my_func(x, y):
     result = x ** y
     return result
@@ -8,9 +8,12 @@ def my_func(x, y):
 
 print(my_func(2, -2))
 
+# Варикнт 2, с оператором **, с использованием lambda-функции
+my_func1 = lambda x, y: x ** y
+print(my_func1(2, -2))
 
-# Варикнт 2, решение с помощью цикла
 
+# Варикнт 3, решение с помощью цикла
 def my_func2(x, y):
     y = abs(y)
     for i in range(y):
