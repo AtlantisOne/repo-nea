d1546ef476fedc12f16ea9ba2555fda057ff9b5b
Evgen <eanikulin@yandex.ru>
2021-01-26 10:57:59 +0500

    //Homework 5 complete

--- 5_1_task.py
+++ 5_1_task.py
@@ -1,1 +1,10 @@
 # 5_1_task
+
+print('Введите данные и нажмите enter, по окончании введите q для выхода или пустую строку')
+with open('task5_1.txt', 'w') as file_object:
+    while True:
+        user_answer = input('Введите данные:')
+        file_object.writelines(user_answer + '\n')
+        if user_answer == '' or user_answer == 'q':
+            print('Выход из программы')
+            break
--- 5_7_task.py
+++ 5_7_task.py
@@ -1,1 +1,30 @@
 # 5_7_task
+import json
+
+my_dict = {}
+average_profit_dict = {}
+average_profit = 0
+profit = 0
+
+with open('task5_7.txt', encoding='utf-8') as out_file_object:
+    i = 0
+    for firm in out_file_object:
+        i += 1
+        name, firm, income, expenses = firm.split()
+        profit = int(income) - int(expenses)
+        if profit <= 0:
+            i -= 1
+        else:
+            average_profit += profit
+        my_dict[name] = profit
+    average_profit /= i
+    average_profit_dict['average_profit'] = round(average_profit)
+    my_base = print([my_dict, average_profit_dict])
+
+with open('task5_7.json', "w", encoding='utf-8') as write_f:
+    js_str = json.dump([my_dict, average_profit_dict], write_f, ensure_ascii=False)
+
+with open('task5_7.json', encoding='utf-8') as read_f:
+    data = json.load(read_f)
+    print(data)
+    print(type(data))
