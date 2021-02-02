08b3dbd575fb6abe5c8ff552deb626f23336d8c1
Evgen <eanikulin@yandex.ru>
2021-02-02 15:51:27 +0500

    //Homework 7 complete

--- 7_2_task.py
+++ 7_2_task.py
@@ -1,1 +1,35 @@
 # 7_2_task
+from abc import ABC, abstractmethod
+
+
+class Dress(ABC):
+
+    def __init__(self, parameters):
+        self.parameters = parameters
+
+    def __add__(self, other):
+        return self.parameters + other.parameters
+
+    @abstractmethod
+    def expense_textile(self):
+        pass
+
+
+class Coat(Dress):
+    @property
+    def expense_textile(self):
+        return round(self.parameters / 6.5 + 0.5)
+
+
+class Suit(Dress):
+    @property
+    def expense_textile(self):
+        return round(2 * self.parameters + 0.3)
+
+
+c = Coat(400)
+print('Затраты ткани на пиджак:', c.expense_textile)
+
+s = Suit(55)
+print('Затраты ткани на костюм:', s.expense_textile)
+print('Общие затраты ткани:', c.expense_textile + s.expense_textile)
--- 7_3_task.py
+++ 7_3_task.py
@@ -1,7 +1,40 @@
 # 7_3_task
-class TownCar(Car):
-    def show_speed(self):
-        if self.speed > 60:
-            return f'\nExcess of speed, your speed {self.speed}'
+
+class Cell:
+    def __init__(self, count_cell):
+        self.count_cell = int(count_cell)
+
+    def __add__(self, other):
+        return f'Объединение двух клеток (сложение): {self.count_cell + other.count_cell}'
+
+    def __sub__(self, other):
+        if self.count_cell - other.count_cell > 0:
+            return f'Участвуют две клетки (вычитание): {self.count_cell - other.count_cell}'
         else:
-            super().show_speed()
+            return f'Вычитаниедвух клеток невозможно, так как разность отрицательная: {self.count_cell - other.count_cell}'
+
+    def __mul__(self, other):
+        return f'Создается общая клетка из двух (умножение): {self.count_cell * other.count_cell}'
+
+    def __floordiv__(self, other):
+        if other.count_cell == 0:
+            return 'Нет смысла, если вторая клетка отсутствует'
+        else:
+            return f'Создается общая клетка из двух (деление): {self.count_cell // other.count_cell}'
+
+    def make_order(self, num):
+        my_str = ''
+        for i in range(int(self.count_cell / num)):
+            my_str += f'{"*" * num} \\n'
+        my_str += f'{"*" * (self.count_cell % num)}'
+        return my_str
+
+
+с_1 = Cell(input('Введите количество ячеек: '))
+с_2 = Cell(input('Введите количество ячеек: '))
+print(с_1 + с_2)
+print(с_1 - с_2)
+print(с_1 * с_2)
+print(с_1 // с_2)
+print(с_1.make_order(5))
+print(с_2.make_order(5))
