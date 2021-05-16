cece1e283643c89ba50414c1d5eb651e17e011e2
Evgen <eanikulin@yandex.ru>
2021-05-17 00:29:07 +0500

    // Homework 9 complete UPD

--- 09_Lesson/Homework_9_vk.sql
+++ 09_Lesson/Homework_9_vk.sql
@@ -1,37 +1,12 @@
---- В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции
-
-START TRANSACTION;      --- включаем транзакцию
-INSERT INTO sample.users (SELECT * FROM shop.users WHERE shop.users.id = 1);   --- перемещаем id из таблицы shop.users в таблицу sample.users
-COMMIT;  --- создаем соммит
-
-
---- Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.
-
-CREATE OR REPLACE VIEW v AS                                                             --- создаем представление
-  SELECT products.name AS p_name, catalogs.name AS cat_name FROM products,catalogs      --- создаем псевдонимы
-    WHERE products.catalog_id = catalogs.id;                                            --- накладываем условие
- --------
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
---1.В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
+  
+-- 1.В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
 START TRANSACTION;
   INSERT INTO sample.users 
  (SELECT * FROM shop.users WHERE id=1);
   DELETE FROM shop.users WHERE id=1;
 COMMIT;
 
---2.Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.
+-- 2.Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.
 CREATE OR REPLACE VIEW v AS catalogs_products AS
 SELECT products.name AS product,
        catalogs.name AS catalog
@@ -41,7 +16,7 @@ JOIN
 catalogs
 ON products.catalog_id=catalogs.id;
    
---------------Практическое задание по теме “Хранимые процедуры и функции, триггеры"-----
+-- Практическое задание по теме “Хранимые процедуры и функции, триггеры"-----
 --1.Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
 
 SET GLOBAL log_bin_trust_function_creators = 1;
@@ -71,27 +46,28 @@ BEGIN
 	END CASE;
 END//
 DELIMITER ;
-
 SELECT hello();
 
---2.В таблице products есть два текстовых поля: name с названием товара и description с его описанием. Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
+-- 2.В таблице products есть два текстовых поля: name с названием товара и description с его описанием. Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
 
 DELIMITER //
 
 CREATE TRIGGER trg_products_insert_check BEFORE INSERT ON products
 FOR EACH ROW
 BEGIN
-  IF ISNULL(NEW.name) && ISNULL(NEW.desription) THEN
-     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Name and desription can`t be null';
+  IF NEW.name IS NULL AND NEW.desription IS NULL THEN
+     SIGNAL SQLSTATE '45000'
+     SET MESSAGE_TEXT = 'Name and desription can`t be null';
   END IF; 
 END//
 
 CREATE TRIGGER trg_products_update_check BEFORE UPDATE ON products
 FOR EACH ROW
 BEGIN
-  IF ISNULL(NEW.name) && ISNULL(NEW.desription) THEN
-     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Name and desription can`t be null';
+  IF NEW.name IS NULL AND NEW.desription IS NULL THEN
+     SIGNAL SQLSTATE '45000'
+     SET MESSAGE_TEXT = 'Name and desription can`t be null';
   END IF; 
-END//
+END///
 
 DELIMITER ;
